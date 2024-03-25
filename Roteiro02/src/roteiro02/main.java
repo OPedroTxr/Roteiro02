21946558dce18632f467ae0e6133e4001049c1fa
PedroTxr <83319342+OPedroTxr@users.noreply.github.com>
2024-03-25 09:26:33 -0300

    01

---
+++ Roteiro02/src/roteiro02/main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package roteiro02;
+
+/**
+ *
+ * @author 15512921657
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
